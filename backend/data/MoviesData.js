44160f527364092c36b0b78c8613e2cd1f231826
Sone <sthaya@gmail.com>
2021-01-09 19:20:16 -0800

    created a backend server to get list of movies

---
+++ backend/data/MoviesData.js
@@ -0,0 +1,134 @@
+const movies = [
+  {
+    _id: "1",
+    Title: "The Shawshank Redemption",
+    Year: "1994",
+    Rated: "R",
+    Released: "14 Oct 1994",
+    Runtime: "142 min",
+    Genre: "Drama",
+    Director: "Frank Darabont",
+    Writer:
+      'Stephen King (short story "Rita Hayworth and Shawshank Redemption"), Frank Darabont (screenplay)',
+    Actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+    Plot:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    Language: "English",
+    Country: "USA",
+    Awards: "Nominated for 7 Oscars. Another 21 wins & 36 nominations.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    Ratings: [
+      {
+        Source: "Internet Movie Database",
+        Value: "9.3/10",
+      },
+      {
+        Source: "Rotten Tomatoes",
+        Value: "91%",
+      },
+      {
+        Source: "Metacritic",
+        Value: "80/100",
+      },
+    ],
+    Metascore: "80",
+    imdbRating: "9.3",
+    imdbVotes: "2,321,344",
+    imdbID: "tt0111161",
+    Type: "movie",
+    DVD: "N/A",
+    BoxOffice: "$28,699,976",
+    Production: "Columbia Pictures, Castle Rock Entertainment",
+    Website: "N/A",
+    Response: "True",
+  },
+  {
+    _id: "2",
+    Title: "Tenet",
+    Year: "2020",
+    Rated: "PG-13",
+    Released: "03 Sep 2020",
+    Runtime: "150 min",
+    Genre: "Action, Sci-Fi",
+    Director: "Christopher Nolan",
+    Writer: "Christopher Nolan",
+    Actors: "Juhan Ulfsak, Jefferson Hall, Ivo Uukkivi, Andrew Howard",
+    Plot:
+      "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+    Language: "English, Russian, Ukrainian, Estonian",
+    Country: "UK, USA",
+    Awards: "3 wins & 6 nominations.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg",
+    Ratings: [
+      {
+        Source: "Internet Movie Database",
+        Value: "7.6/10",
+      },
+      {
+        Source: "Rotten Tomatoes",
+        Value: "70%",
+      },
+      {
+        Source: "Metacritic",
+        Value: "69/100",
+      },
+    ],
+    Metascore: "69",
+    imdbRating: "7.6",
+    imdbVotes: "195,389",
+    imdbID: "tt6723592",
+    Type: "movie",
+    DVD: "N/A",
+    BoxOffice: "$57,800,000",
+    Production: "Syncopy, Warner Bros. Pictures",
+    Website: "N/A",
+    Response: "True",
+  },
+  {
+    _id: "3",
+    Title: "The Avengers",
+    Year: "2012",
+    Rated: "PG-13",
+    Released: "04 May 2012",
+    Runtime: "143 min",
+    Genre: "Action, Adventure, Sci-Fi",
+    Director: "Joss Whedon",
+    Writer: "Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)",
+    Actors: "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+    Plot:
+      "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    Language: "English, Russian, Hindi",
+    Country: "USA",
+    Awards: "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    Ratings: [
+      {
+        Source: "Internet Movie Database",
+        Value: "8.0/10",
+      },
+      {
+        Source: "Rotten Tomatoes",
+        Value: "91%",
+      },
+      {
+        Source: "Metacritic",
+        Value: "69/100",
+      },
+    ],
+    Metascore: "69",
+    imdbRating: "8.0",
+    imdbVotes: "1,252,715",
+    imdbID: "tt0848228",
+    Type: "movie",
+    DVD: "N/A",
+    BoxOffice: "$623,357,910",
+    Production: "Marvel Studios",
+    Website: "N/A",
+    Response: "True",
+  },
+];
+
+module.exports = movies;
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const movies = require("./data/MoviesData");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/movies", (req, res) => {
+  res.json(movies);
+});
+
+app.get("/api/movies/:id", (req, res) => {
+    const movie = movies.find(p => p._id === req.params.id);
+
+    res.json(movie);
+  });
+
+app.listen(5000, console.log("server running on port 5000"));
