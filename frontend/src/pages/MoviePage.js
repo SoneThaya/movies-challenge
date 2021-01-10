44cbacab7f0a2011040285cdbce59a668e85d4fb
Sone <sthaya@gmail.com>
2021-01-09 19:03:44 -0800

    created movie page to display a single movie

--- frontend/src/pages/MoviePage.js
+++ frontend/src/pages/MoviePage.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import moviesData from "../MoviesData";
 
-const MoviePage = () => {
-    return (
-        <div>
-            movie
-        </div>
-    )
-}
+const MoviePage = ({ match }) => {
+  const movie = moviesData.find((m) => m._id === match.params.id);
 
-export default MoviePage
+  return (
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={movie.Poster} alt={movie.Title} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>{movie.Title}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>{movie.Year}</ListGroup.Item>
+            <ListGroup.Item>
+              <strong>{movie.Plot}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>Director: {movie.Director}</ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Cast: {movie.Actors}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>Metascore: {movie.Metascore}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Button className="btn-block" type="button">
+                  Nominate
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default MoviePage;
