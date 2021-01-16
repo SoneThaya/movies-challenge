bc4a987875dd5dd7ef26318488c8881deb62cbd4
Sone <sthaya@gmail.com>
2021-01-16 12:56:52 -0800

    setup server file for delopyment and ran npm run build

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,20 @@ app.use(express.json());
 app.use("/api/movies", movieRoutes);
 app.use("/api/users", userRoutes);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
@@ -35,9 +49,3 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
-
-// import movieRoutes from "./routes/movieRoutes.js";
-// import userRoutes from "./routes/userRoutes.js";
-
-// app.use("/api/movies", movieRoutes);
-// app.use("/api/users", userRoutes);
