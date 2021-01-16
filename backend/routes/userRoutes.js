16ec00f037abe7f3a8a0376d04d55df0ed79b2cb
Sone <sthaya@gmail.com>
2021-01-15 17:03:46 -0800

    refactored code

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,29 +3,23 @@ const router = express.Router();
 import {
   authUser,
   registerUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-  deleteUser,
-  getUserById,
-  updateUser,
   getUserNominations,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.route("/").post(registerUser).get(protect, admin);
 router.post("/login", authUser);
-router
-  .route("/profile")
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
-// need a post nominations route
+// router
+//   .route("/profile")
+//   .get(protect, getUserProfile)
+//   .put(protect, updateUserProfile);
+// // need a post nominations route
 router.route("/nominations").get(protect, getUserNominations);
 
-router
-  .route("/:id")
-  .delete(protect, admin, deleteUser)
-  .get(protect, admin, getUserById)
-  .put(protect, admin, updateUser);
+// router
+//   .route("/:id")
+//   .delete(protect, admin, deleteUser)
+//   .get(protect, admin, getUserById)
+//   .put(protect, admin, updateUser);
 
 export default router;
